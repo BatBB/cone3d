714c243ad05c77e8c61000e2af0a11611c905133
Bator B <batorbb@gmail.com>
2023-09-06 17:26:26 +0200

    feat: add backend and api calc triangulations

---
+++ README.md
@@ -0,0 +1,27 @@
+# Cone 3D
+
+A test client-server web application which can display a simple cone in a 3D view.
+
+## Getting Started
+
+Follow these steps to set up and run Cone 3D on your local machine:
+
+1. **Clone the Repository:**
+`$ git clone https://github.com/BatBB/cone3d.git`
+2. **Navigate to the Project Folder:**
+`$ cd cone3d`
+3. **Install Dependencies:**
+`$ npm install`
+4. **Start the Application:**
+`$ npm run start:all`
+Alternatively, you can start the server and client separately:
+
+   * Start the server:
+`$ npm run server`
+   * Start the client:
+`$ npm run client`
+
+5. **Access the Client:**
+<http://127.0.0.1:4200>
+6. **Send Requests:**
+<http://127.0.0.1:3000>
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+app.post('/calc-cone', (req, res) => {
+  const { height, radius, segments } = req.body
+
+  const triangulation = getTriangulationCone(height, radius, segments)
+
+  res.json(triangulation)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+function getTriangulationCone(height, radius, segments) {
+  const triangulation = []
+  const vertexA = {
+    x: 0,
+    y: 0,
+    z: height,
+  }
+
+  for (let i = 0; i < segments; i++) {
+    const angle1 = (2 * Math.PI * i) / segments
+    const nextI = i === segments - 1 ? 0 : Math.PI * (i + 1)
+    const angle2 = (2 * nextI) / segments
+    const p1 = {
+      x: radius * Math.cos(angle1),
+      y: radius * Math.sin(angle1),
+      z: 0,
+    }
+    const p2 = {
+      x: radius * Math.cos(angle2),
+      y: radius * Math.sin(angle2),
+      z: 0,
+    }
+
+    triangulation.push([vertexA, p1, p2])
+  }
+  return triangulation
+}
